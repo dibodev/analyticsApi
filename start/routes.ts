a05be25ca8c6a7588a0d16b8e8d51061ba2cb6f5
Léo Guillaume <leo.guillaume@gmail.com>
2023-06-08 18:37:06 +0200

    update: test ci

--- start/routes.ts
+++ start/routes.ts
@@ -2,7 +2,7 @@ import Route from '@ioc:Adonis/Core/Route'
 import Application from '@ioc:Adonis/Core/Application'
 
 Route.get('/', async () => {
-  return { success: true }
+  return { success: true, version: 1 }
 })
 
 Route.post('/generate-script', 'WebsitesController.generateScript')
