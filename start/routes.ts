1ad5b22b88ed2b7d7f7b21b4cf1d91f9443ecdd3
Léo Guillaume <leo.guillaume@gmail.com>
2023-06-08 22:11:17 +0200

    fix: public path

--- start/routes.ts
+++ start/routes.ts
@@ -5,9 +5,17 @@ Route.get('/', async () => {
   return { success: true, version: 2 }
 })
 
+Route.get('/test', async () => {
+  return { test: true }
+})
+
 Route.post('/generate-script', 'WebsitesController.generateScript')
 Route.post('/collect', 'WebsitesController.collectData')
 Route.get('/websites', 'WebsitesController.all')
+
+Route.get('/tracker', async () => {
+  return Application.publicPath('tracker.js')
+})
 
 Route.get('/tracker.js', async ({ response }) => {
   return response.download(Application.publicPath('tracker.js'))
