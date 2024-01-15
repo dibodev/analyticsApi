d515dd86fc9bb2f9c0d7e054e44d7308c5ff04de
Léo Guillaume <leo.guillaume@epitech.eu>
2024-01-15 11:38:01 +0100

    feat: add backup database ci

--- database/seeders/5_VisitorEventSeeder.ts
+++ database/seeders/5_VisitorEventSeeder.ts
@@ -1,8 +1,13 @@
 import BaseSeeder from '@ioc:Adonis/Lucid/Seeder'
 import VisitorEventFactory from 'Database/factories/VisitorEventFactory'
+import VisitorEvent from 'App/Models/VisitorEvent'
 
 export default class extends BaseSeeder {
-  public async run() {
-    await VisitorEventFactory.createMany(2000)
+  public async run(): Promise<void> {
+    const visitorEventsMax: number = 2000
+    const visitorEvents: Array<VisitorEvent> = await VisitorEvent.all()
+    if (visitorEvents.length < visitorEventsMax) {
+      await VisitorEventFactory.createMany(visitorEventsMax)
+    }
   }
 }
