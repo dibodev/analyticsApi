a1dd3a4aab70a4e9dbe97010c4fd95fdf1e2d344
Léo Guillaume <leo.guillaume@epitech.eu>
2024-01-31 01:51:55 +0100

    fix: ip prod

--- app/Controllers/Http/VisitorTrackingController.ts
+++ app/Controllers/Http/VisitorTrackingController.ts
@@ -1,6 +1,5 @@
 import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 import VisitorTrackingDataService from 'App/Services/VisitorTrackingDataService'
-import type { VisitorData } from 'App/Services/VisitorTrackingDataService'
 import VisitorTrackingDataValidator from 'App/Validators/VisitorTrackingDataValidator'
 import type { VisitorTrackingData } from 'App/Validators/VisitorTrackingDataValidator'
 import IPService from 'App/Services/IPService'
@@ -8,19 +7,8 @@ import type { IPApiResponse } from 'App/Services/IPService'
 import { RequestContract } from '@ioc:Adonis/Core/Request'
 
 export default class VisitorTrackingController {
-  public async join({ request }: HttpContextContract): Promise<VisitorData> {
-    const visitorTrackingData: VisitorTrackingData = await request.validate(
-      VisitorTrackingDataValidator
-    )
-
-    const userAgent: string | null = this.getUserAgent(visitorTrackingData, request)
-
-    const clientIpInfos: IPApiResponse | null = await IPService.getClientIpInfo(request)
-
-    return await VisitorTrackingDataService.collectVisitorData(clientIpInfos, {
-      ...visitorTrackingData,
-      userAgent,
-    })
+  public async join({ request }: HttpContextContract): Promise<string> {
+    return IPService.getIpFromHeaders(request)
   }
   public async leave({ request, response }: HttpContextContract): Promise<void> {
     const visitorTrackingData: VisitorTrackingData = await request.validate(
